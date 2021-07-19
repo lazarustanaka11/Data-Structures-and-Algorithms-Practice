40270719c990e5152cbc1d62d091bf8a5f2b54c3
lazarustanaka11 <lazarusmtake@gmail.com>
2021-07-19 14:54:25 +0200

    'forth'

---
+++ isPalindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        num = x
+        reverse = 0
+
+        while num!=0:
+            reverse = reverse*10 + num % 10
+            num //= 10
+        return reverse==x
